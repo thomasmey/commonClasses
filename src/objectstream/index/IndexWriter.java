6601c352e2c573da158ab8ee5ca215e351f87709
Thomas Meyer <thomas@m3y3r.de>
2012-10-27 17:22:53 +0200

    Common classes. Initial commit contains file base ObjectStream index classes.

---
+++ src/objectstream/index/IndexWriter.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2012 Thomas Meyer
+ */
+
+package objectstream.index;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class IndexWriter<E> implements IndexConstants {
+
+	private final File inputFile;
+	private final Comparator<E> comparator;
+	private final String indexName;
+	private final List<E> list = new ArrayList<E>();
+	private int indexCounter;
+	
+	private static final int MAX_ELEMENTS = 50000;
+
+	public IndexWriter(File inputFile, String indexName, Comparator<E> comparator) {
+		this.inputFile = inputFile;
+		this.indexName = indexName;
+		this.comparator = comparator;
+	}
+
+	public boolean write(E e) {
+
+		boolean rc = false;
+		rc = list.add(e);
+
+		writeElements(MAX_ELEMENTS);
+
+		return rc;
+	}
+
+	private void writeElements(int maxSize) {
+		// pre sort elements
+		if(list.size() > maxSize) {
+			Collections.sort(list, comparator);
+			for(E el: list) {
+				ObjectOutputStream out = null;
+				try {
+					out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(inputFile.toString() + '.' + indexName + '.' + filePostfix + '.' + indexCounter++)));
+					out.writeObject(el);
+				} catch (IOException ex) {
+					System.out.println(ex);
+				} finally {
+					if(out != null)
+						try {
+							out.close();
+						} catch (IOException e1) {}
+				}
+			}
+			list.clear();
+		}
+	}
+
+	public void close() {
+		writeElements(indexCounter);
+	}
+}
